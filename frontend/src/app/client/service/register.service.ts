050a558dd6d71aef01d9cf1d2340d89bda96d89f
Guyoguyes <g.abuba43@gmail.com>
2020-02-17 12:05:11 +0300

    service debugged ok

---
+++ frontend/src/app/client/service/register.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+uri = 'http://localhost:4000/client';
+
+  constructor(private http: HttpClient) { }
+
+  //Add Business
+  addClient(first_name, last_name, email, mobile, city, password){
+    const obj = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      mobile: mobile,
+      city: city,
+      password: password
+    };
+    console.log(obj);
+    this.http.post(`${this.uri}/register-client`, obj)
+        .subscribe(res => console.log('done'))
+  }
+}
