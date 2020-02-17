ed4c98bdc82e6780bec699e1942d6b422edaa1a7
mandalorian <mandalorian@localhost.localdomain>
2020-02-17 22:24:39 +0300

    new reg form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   // {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent},
